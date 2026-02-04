f86a18c35f179cc3a345e5bb78bbf3619f9282fe
atiq <muhammadatiq757@gmail.com>
2026-02-04 21:28:09 +0600

    implement user management features with error handling and response utilities

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import express from "express";
 import type { Request, Response } from "express";
+import cors from "cors";
+import { router } from "./routes";
+import { globalErrorHandler } from "./middlewares/globalErrorHandler";
+import notFound from "./middlewares/notFound";
 
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+app.use("/api/v1", router);
+
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
-    message: "Welcome to tour Management system Backend",
+    message: "Welcome to Management system Backend",
   });
 });
 
-export default app
+// global error handler
+app.use(globalErrorHandler);
+app.use(notFound);
+
+export default app;
