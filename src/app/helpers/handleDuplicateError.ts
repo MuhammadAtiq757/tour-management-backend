a9f30feb4af1a3288291d82497d7b1a06c4d7e61
atiq <muhammadatiq757@gmail.com>
2026-02-27 16:34:49 +0600

    implement division module with controller, service, routes, and validation schemas

--- src/app/helpers/handleDuplicateError.ts
+++ src/app/helpers/handleDuplicateError.ts
@@ -3,9 +3,11 @@ import { TGenericErrorResponse } from "../interfaces/error.types"
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const handlerDuplicateError = (err: any): TGenericErrorResponse => {
     const matchedArray = err.message.match(/"([^"]*)"/)
+    const keyValue = err?.keyValue ? Object.values(err.keyValue)[0] : undefined
+    const duplicateValue = matchedArray?.[1] ?? (keyValue !== undefined ? String(keyValue) : "Value")
 
     return {
         statusCode: 400,
-        message: `${matchedArray[1]} already exists!!`
+        message: `${duplicateValue} already exists!!`
     }
 }
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express"
 import { UserRoutes } from "../modules/user/user.routes"
 import { AuthRoutes } from "../modules/auth/auth.routes"
+import { DivisionRoutes } from "../modules/division/division.routes"
 
 
 export const router = Router()
@@ -13,6 +14,10 @@ const moduleRoutes = [
     {
         path: "/auth",
         route: AuthRoutes
+    },
+    {
+        path: "/division",
+        route: DivisionRoutes
     }
     // {
     //     path: "/tour",
