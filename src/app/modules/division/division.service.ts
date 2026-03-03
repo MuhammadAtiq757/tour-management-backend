69d7d4166e774108dd0331165777df382cb296e1
atiq <muhammadatiq757@gmail.com>
2026-03-03 09:23:26 +0600

    refactor: streamline slug generation logic in division model and service

--- src/app/modules/division/division.service.ts
+++ src/app/modules/division/division.service.ts
@@ -9,16 +9,6 @@ const createDivision = async (payload: IDivision) => {
     }
 
 
-    const baseSlug = payload.name.toLowerCase().split(" ").join("-")
-    let slug = `${baseSlug}-division`
-
-    let counter = 0;
-    while (await Division.exists({ slug })) {
-        slug = `${slug}-${counter++}` // dhaka-division-2
-    }
-
-    payload.slug = slug;
-
     const division = await Division.create(payload);
 
     return division
@@ -58,18 +48,6 @@ const updateDivision = async (id: string, payload: Partial<IDivision>) => {
     if (duplicateDivision) {
         throw new Error("A division with this name already exists.");
     }
-
-    // if (payload.name) {
-    //     const baseSlug = payload.name.toLowerCase().split(" ").join("-")
-    //     let slug = `${baseSlug}-division`
-
-    //     let counter = 0;
-    //     while (await Division.exists({ slug })) {
-    //         slug = `${slug}-${counter++}` // dhaka-division-2
-    //     }
-
-    //     payload.slug = slug
-    // }
 
     const updatedDivision = await Division.findByIdAndUpdate(id, payload, { new: true, runValidators: true })
 
