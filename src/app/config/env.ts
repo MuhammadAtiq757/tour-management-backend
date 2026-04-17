7deb08072ecf32741f1135292a529c00aca982bf
atiq <muhammadatiq757@gmail.com>
2026-04-17 21:19:13 +0600

    feat: implement stats module with booking, payment, user, and tour statistics endpoints and services

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -29,6 +29,7 @@ interface EnvConfig {
     SSL_SUCCESS_BACKEND_URL: string;
     SSL_FAIL_BACKEND_URL: string;
     SSL_CANCEL_BACKEND_URL: string;
+    SSL_IPN_URL: string;
   };
 
   CLOUDINARY: {
@@ -127,6 +128,7 @@ const loadEnvVariables = (): EnvConfig => {
       SSL_SUCCESS_BACKEND_URL: process.env.SSL_SUCCESS_BACKEND_URL as string,
       SSL_FAIL_BACKEND_URL: process.env.SSL_FAIL_BACKEND_URL as string,
       SSL_CANCEL_BACKEND_URL: process.env.SSL_CANCEL_BACKEND_URL as string,
+      SSL_IPN_URL: process.env.SSL_IPN_URL as string,
     },
     CLOUDINARY: {
       CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
